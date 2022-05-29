ba48ed4ba947272a0cef262ac2c0efe7488a6b6c
QuocTuanIT <kutuanonline199@gmail.com>
2022-05-29 20:40:13 +0700

    edit setting

--- RestDjangoEcom/settings.py
+++ RestDjangoEcom/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#@y*%v5g1p53=fi-2_g)yw#ti+%5j5(m#=_ua$9-rk*m78#5j'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-n#@y*%v5g1p53=fi-2_g)yw#ti+%5j5(m#=_ua$9-rk*m78#5j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['https://tqt-rest-djshop.herokuapp.com']
 
